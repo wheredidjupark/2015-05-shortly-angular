222580e37c3c32819168c1badeb3459de1803b69
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-08-31 16:37:44 -0700

    added .addLink function to the ShortenController

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -3,4 +3,8 @@ angular.module('shortly.shorten', [])
 .controller('ShortenController', function ($scope, $location, Links) {
   // Your code here
   $scope.link = {};
+  $scope.addLink = function(){
+  	
+  };
+
 });
