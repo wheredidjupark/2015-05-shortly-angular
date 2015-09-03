8bede9361bd8363c759adc058dc4ca6cc327feba
Ju Hyeon Park <wheredidjupark@gmail.com>
2015-09-02 21:28:34 -0700

    modified minor features in /shorten. 500 Internal Server Error still persists

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -5,7 +5,12 @@ angular.module('shortly.shorten', [])
   $scope.link = {};
 
   $scope.addLink = function(link){
-  	Links.addLink(link);
+  	console.log("adding the following link:" + $scope.link.url);
+  	Links.addLink(link).then(function(){
+  		console.log("successfully added" + $scope.link.url);
+  	}, function(error){
+  		console.log(error);
+  	});
   };
 
 
